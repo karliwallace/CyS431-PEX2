2343c0bf173c95920f58219835ffcc15ea9f01c2
karliwallace <c23karli.wallace@afacademy.af.edu>
2022-04-27 11:14:59 -0600

    PEX2 - Setup

---
+++ main.c
@@ -0,0 +1,5 @@
+//
+// Created by student on 4/27/22.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//  Karli Wallace
+// Documentation: None
+
+#ifndef CYS431_PEX2_MAIN_H
+#define CYS431_PEX2_MAIN_H
+#endif //CYS431_PEX2_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+
