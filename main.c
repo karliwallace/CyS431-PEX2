94c3e044ca98b54c774ebdfee7f133c3f718f403
karliwallace <c23karli.wallace@afacademy.af.edu>
2022-05-03 15:33:02 -0600

    PEX2 - File Reading

--- main.c
+++ main.c
@@ -3,3 +3,28 @@
 //
 
 #include "main.h"
+
+
+main() {
+    FILE *fp = fopen("samplefile.txt", "r");  //read only file
+    char *fileData;
+    if (fp) {
+        fseek(fp, 0, SEEK_END);
+        long len = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+        fileData = malloc(len);
+        if (fileData) {
+            fread(fileData, 1, len, fp);
+        }
+        fclose(fp);
+    }
+    printf("%s", fileData); //test checking
+
+//    char *readIn = NULL;
+//    while(1) {
+//        fgets(readIn, 1000000, fp);    //
+//    }
+//    if (fp)
+
+}
+
